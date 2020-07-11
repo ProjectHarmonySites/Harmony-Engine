a73d61f88bbbc261dc9f3a9647b82fb9f49cdac8
Nick <nickfanelli5@gmail.com>
2020-07-11 17:09:41 -0400

    Finished Lander Page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,43 @@
+/********************* Global Methods *********************/
+
+const sponsorsList = `
+
+	First, Last
+	Company Name
+	Sample Company
+	First, Last
+	First, Last
+	Sample Company
+	Sample Company
+	Company Name
+
+`;
+
+const thanks = document.querySelector(".thanks");
+
+function scrollToTop() {
+	document.body.scrollTop = 0; // Safari
+	document.documentElement.scrollTop = 0; // Everything else
+}
+
+function loadSponsors() {
+	if(thanks != null) {
+		let list = document.querySelector(".thanks ul");
+		let sponsorsArray = sponsorsList.split("\n");
+
+		for(let i = 0; i < sponsorsArray.length; i++) {
+			let item = sponsorsArray[i];
+			if(item === "") continue;
+
+			list.innerHTML += `<li>${item}</li>`;
+		}
+	}
+}
+
+this.loadSponsors();
+
+/********************* Navbar *********************/
+
 const burger 		= document.querySelector("nav .burger");
 const navContent 	= document.querySelector("nav .content");
 
@@ -21,3 +61,4 @@ function removeTranslate() {
 }
 
 window.onresize = removeTranslate;
+
