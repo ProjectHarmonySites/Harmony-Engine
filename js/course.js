178717e683c793f7790083604368a87528ac5734
Nick <nickfanelli5@gmail.com>
2020-08-26 10:48:05 -0400

    Pausing for the time-being to work on the engine so I actually have something to put in the getting started

---
+++ js/course.js
@@ -0,0 +1,6 @@
+const COURSE_INFO = document.querySelector("#header .data-wrapper");
+const COURSE_SECTION = document.querySelector("#header .course");
+
+function startCourse() {
+    COURSE_INFO.style.display = "none";
+}
