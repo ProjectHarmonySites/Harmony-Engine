0f092c421027023343d63327c24df0663e2e4b8a
Nick <nickfanelli5@gmail.com>
2020-08-25 20:42:18 -0400

    Cleaned it up a bit and added the instructions

--- js/courses.js
+++ js/courses.js
@@ -1,6 +1,7 @@
 const COURSE_NAME = document.querySelector("#header .data h1");
 const COURSE_DESC = document.querySelector("#header .data .desc");
 const COURSE_BUTTON = document.querySelector("#header .data button");
+const COURSE_INSTRUCTIONS = document.querySelector("#header .data .instructions");
 
 const DIFF_SLIDER = document.querySelector(".difficulty-slider");
 const DIFF_LABEL = document.querySelector(".difficulty-slider h3");
@@ -16,7 +17,9 @@ function setCourse(course) {
     selectedCourse = course;
     selectedCourse.classList.add("selected");
 
+    COURSE_INSTRUCTIONS.style.display = "none";
     COURSE_BUTTON.style.display = "block";
+
     COURSE_NAME.innerHTML = selectedCourse.getAttribute("name");
     COURSE_DESC.innerHTML = selectedCourse.getAttribute("desc");
 
